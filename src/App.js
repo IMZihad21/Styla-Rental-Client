5555808405cf728696fb7e9c39b59b946f1d5261
ZèD <imzihad@gmail.com>
2021-10-29 01:59:34 +0600

    Initial setup for react router
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} exact />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
