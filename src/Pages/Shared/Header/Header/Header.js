e616eeb6548696ea7b2462ce0fadcd84b28dfe13
ZèD <imzihad@gmail.com>
2021-10-29 19:56:12 +0600

    Added a template modal for cart
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Pages/Shared/Header/Header/Header.js
+++ src/Pages/Shared/Header/Header/Header.js
@@ -1,7 +1,23 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement("#root");
 
 const Header = () => {
+    const [ cartOpen, setCartOpen ] = useState(false);
+    const toggleCartModal = () => {
+        setCartOpen(!cartOpen);
+    };
+    const modalStyle = {
+        content: {
+            top: '30%',
+            left: '30%',
+            right: '30%',
+            bottom: '30%',
+        },
+    };;
+
     return (
         <div className="flex justify-between px-2 md:px-16 pt-4">
             <div className="h-14 my-auto my-auto bg-gray-400 rounded-xl">
@@ -15,9 +31,18 @@ const Header = () => {
                         <button className="px-10 py-3 my-auto bg-red-200 rounded-xl">Sign In</button>
                     </Link>
                 </div>
-                <button>
+                <button onClick={toggleCartModal}>
                     <i className="fas fa-shopping-cart my-auto text-4xl pl-4"></i>
                 </button>
+                <Modal
+                    isOpen={cartOpen}
+                    onRequestClose={toggleCartModal}
+                    style={modalStyle}
+                >
+                    <div className="rounded p-10">
+                        {/* This is cart Placeholder */}
+                    </div>
+                </Modal>
                 <button>
                     <i className="fas fa-search my-auto text-3xl pl-4"></i>
                 </button>
