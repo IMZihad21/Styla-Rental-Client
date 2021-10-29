b916b0cc9daa76d63f1b8599c35c582caf04b120
ZèD <imzihad@gmail.com>
2021-10-30 02:14:22 +0600

    Hax to fix Pin location on map
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Pages/Home/LocationMap/LocationMap.js
+++ src/Pages/Home/LocationMap/LocationMap.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const LocationMap = () => {
     return (
         <div>
-            <MapContainer className='h-96' center={[ 23.8135, 90.4242 ]} zoom={16} scrollWheelZoom={false}>
+            <MapContainer className='h-96' center={[ 23.8135, 90.4242 ]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                <Marker position={[ 23.8135, 90.4242 ]}>
+                    <Popup>
+                        <div className='p-4 bg-gray-800 text-white rounded-xl'>
+                            <h1 className='text-xl'>Styla Rental Head Office</h1>
+                            <p>Jamuna Future Park</p>
+                        </div>
+                    </Popup>
+                </Marker>
             </MapContainer>
         </div>
     );
--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -3,6 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
+            Nothing here!
         </div>
     );
 };
